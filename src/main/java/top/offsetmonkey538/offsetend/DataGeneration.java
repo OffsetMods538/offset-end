1c0adece5cfb3db4d768d64b964d401243e8f32e
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-03 16:15:06 +0200

    Add data generation

---
+++ src/main/java/top/offsetmonkey538/offsetend/DataGeneration.java
@@ -0,0 +1,12 @@
+package top.offsetmonkey538.offsetend;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DataGeneration implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
