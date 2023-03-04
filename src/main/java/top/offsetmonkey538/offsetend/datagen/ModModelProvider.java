99a62078443a21dbe297b5cdeb4ae6d8dea8afa6
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-04 20:06:33 +0200

    Add texture to end mushroom stew

--- src/main/java/top/offsetmonkey538/offsetend/datagen/ModModelProvider.java
+++ src/main/java/top/offsetmonkey538/offsetend/datagen/ModModelProvider.java
@@ -4,7 +4,9 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.minecraft.data.client.BlockStateModelGenerator;
 import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
 import top.offsetmonkey538.offsetend.registry.ModBlocks;
+import top.offsetmonkey538.offsetend.registry.ModItems;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {
@@ -18,6 +20,6 @@ public class ModModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
-
+        itemModelGenerator.register(ModItems.END_MUSHROOM_STEW, Models.GENERATED);
     }
 }
