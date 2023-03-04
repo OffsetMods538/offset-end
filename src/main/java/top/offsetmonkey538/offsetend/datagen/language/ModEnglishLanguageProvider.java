c7f1e2ace4a7740babd954a38fff64b404b5287e
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-04 20:07:28 +0200

    Add translation to end mushroom stew

--- src/main/java/top/offsetmonkey538/offsetend/datagen/language/ModEnglishLanguageProvider.java
+++ src/main/java/top/offsetmonkey538/offsetend/datagen/language/ModEnglishLanguageProvider.java
@@ -3,6 +3,7 @@ package top.offsetmonkey538.offsetend.datagen.language;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import top.offsetmonkey538.offsetend.registry.ModBlocks;
+import top.offsetmonkey538.offsetend.registry.ModItems;
 import top.offsetmonkey538.offsetend.registry.ModStatusEffects;
 
 public class ModEnglishLanguageProvider extends FabricLanguageProvider {
@@ -13,6 +14,9 @@ public class ModEnglishLanguageProvider extends FabricLanguageProvider {
     @Override
     public void generateTranslations(TranslationBuilder translationBuilder) {
         translationBuilder.add(ModBlocks.END_MUSHROOM, "End Mushroom");
+
+        translationBuilder.add(ModItems.END_MUSHROOM_STEW, "End Mushroom Stew");
+
         translationBuilder.add(ModStatusEffects.END_AFFINITY, "End Affinity");
     }
 }
