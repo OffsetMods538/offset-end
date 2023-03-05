76e3a54fc915c9279f3722835b3310e695e64a7f
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-05 17:20:03 +0200

    Make end water transparent

--- src/client/java/top/offsetmonkey538/offsetend/OffsetEndClient.java
+++ src/client/java/top/offsetmonkey538/offsetend/OffsetEndClient.java
@@ -2,8 +2,12 @@ package top.offsetmonkey538.offsetend;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.util.Identifier;
 import top.offsetmonkey538.offsetend.registry.ModBlocks;
+import top.offsetmonkey538.offsetend.registry.ModFluids;
 
 public class OffsetEndClient implements ClientModInitializer {
 
@@ -11,5 +15,14 @@ public class OffsetEndClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.END_MUSHROOM, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.POTTED_END_MUSHROOM, RenderLayer.getCutout());
+
+		BlockRenderLayerMap.INSTANCE.putFluids(RenderLayer.getTranslucent(), ModFluids.STILL_END_WATER, ModFluids.FLOWING_END_WATER);
+
+
+		FluidRenderHandlerRegistry.INSTANCE.register(ModFluids.STILL_END_WATER, ModFluids.FLOWING_END_WATER, new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"),
+				new Identifier("minecraft:block/water_flow"),
+				0x161616
+		));
 	}
 }
