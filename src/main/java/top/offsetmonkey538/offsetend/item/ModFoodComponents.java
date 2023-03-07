e84dd2cd785421b6e7fb0418d58277d64ae7e8c7
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-07 17:26:26 +0200

    Changed amplifier of the end affinity effect from 1 to 0 because 1 actually means level 2

--- src/main/java/top/offsetmonkey538/offsetend/item/ModFoodComponents.java
+++ src/main/java/top/offsetmonkey538/offsetend/item/ModFoodComponents.java
@@ -11,6 +11,6 @@ public class ModFoodComponents {
     public static final FoodComponent END_MUSHROOM_STEW = new FoodComponent.Builder()
             .hunger(MUSHROOM_STEW.getHunger())
             .saturationModifier(MUSHROOM_STEW.getSaturationModifier())
-            .statusEffect(new StatusEffectInstance(ModStatusEffects.END_AFFINITY, 2400, 1), 1)
+            .statusEffect(new StatusEffectInstance(ModStatusEffects.END_AFFINITY, 2400, 0), 1)
             .build();
 }
