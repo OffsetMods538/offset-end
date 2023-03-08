a04dc1ca48755d0c4d6d0501752d0189404413ed
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-08 16:32:57 +0200

    Give end cows mushrooms

--- src/client/java/top/offsetmonkey538/offsetend/render/entity/EndCowEntityRenderer.java
+++ src/client/java/top/offsetmonkey538/offsetend/render/entity/EndCowEntityRenderer.java
@@ -6,6 +6,7 @@ import net.minecraft.client.render.entity.model.CowEntityModel;
 import net.minecraft.util.Identifier;
 import top.offsetmonkey538.offsetend.entity.EndCowEntity;
 import top.offsetmonkey538.offsetend.render.entity.feature.EndCowEyesFeatureRenderer;
+import top.offsetmonkey538.offsetend.render.entity.feature.EndCowMushroomFeatureRenderer;
 import top.offsetmonkey538.offsetend.render.entity.model.ModEntityModelLayers;
 
 import static top.offsetmonkey538.offsetend.OffsetEnd.id;
@@ -15,6 +16,7 @@ public class EndCowEntityRenderer extends MobEntityRenderer<EndCowEntity, CowEnt
     public EndCowEntityRenderer(EntityRendererFactory.Context context) {
         super(context, new CowEntityModel<>(context.getPart(ModEntityModelLayers.END_COW)), 0.5f);
         addFeature(new EndCowEyesFeatureRenderer<>(this));
+        addFeature(new EndCowMushroomFeatureRenderer<>(this, context.getBlockRenderManager()));
     }
 
     @Override
