3e1e17f1e299db4e14b46256f1e402025d326c64
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2023-03-10 15:47:51 +0200

    We do actually want to spawn particles if the block above is water

--- src/main/java/top/offsetmonkey538/offsetend/fluid/EndWaterFluid.java
+++ src/main/java/top/offsetmonkey538/offsetend/fluid/EndWaterFluid.java
@@ -2,7 +2,6 @@ package top.offsetmonkey538.offsetend.fluid;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.FluidBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.fluid.FlowableFluid;
@@ -39,7 +38,7 @@ public abstract class EndWaterFluid extends FlowableFluid {
 
     @Override
     protected void randomDisplayTick(World world, BlockPos pos, FluidState state, Random random) {
-        if (!state.isStill() || world.getBlockState(pos.up()).isOf(Blocks.AIR)) return;
+        if (!state.isStill()) return;
         if (random.nextInt(10) == 0) world.addParticle(getParticle(), (double)pos.getX() + random.nextDouble(), (double)pos.getY() + random.nextDouble(), (double)pos.getZ() + random.nextDouble(), 0.0, 0.0, 0.0);
         else if (random.nextInt(256) == 0) world.playSound((double)pos.getX() + 0.5, (double)pos.getY() + 0.5, (double)pos.getZ() + 0.5, SoundEvents.ENTITY_ENDERMITE_HURT, SoundCategory.BLOCKS, random.nextFloat() * 0.25f + 0.50f, random.nextFloat() + 0.5f, false);
     }
